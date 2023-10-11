90e8d8e81c15a4131324ccf1b12c35ad932d408d
Damian Szkudlarek <damian.szkudlarek@yuush.com>
2023-10-11 03:38:14 +0200

    Optymalazing and making script more readable

--- script.js
+++ script.js
@@ -7,14 +7,15 @@ const one = document.querySelector('.one');
 const two = document.querySelector('.two');
 
 let convValue;
-console.log(converter.value)
 const convert = () => {
-  if(one.textContent == '°C' && converter.value !== '' && converter.value.match(numbers)) {
-    convValue = Math.floor(((parseInt(converter.value) * 1.8) + 32) * 10) / 10;
-    result.textContent = `${converter.value}°C to ${convValue}°F`
-  } else if(one.textContent == '°F' && converter.value !== '' && converter.value.match(numbers)) {
-    convValue = Math.floor(((parseInt(converter.value) - 32) / 1.8) * 10) / 10;
-    result.textContent = `${converter.value}°F to ${convValue}°C`
+  if(converter.value !== '') {
+    if(one.textContent == '°C') {
+      convValue = (converter.value * 1.8) + 32;
+      result.textContent = `${converter.value}°C to ${convValue.toFixed(1)}°F`
+    } else {
+      convValue = (converter.value - 32) / 1.8;
+      result.textContent = `${converter.value}°F to ${convValue.toFixed(1)}°C`
+    }
   } else {
     result.textContent = 'Musisz podać temperaturę';
   }
@@ -26,16 +27,26 @@ const reset = () => {
 }
 
 const change = () => { 
-  if(one.textContent == '°C') {
-    one.textContent = '°F';
-    two.textContent = '°C';
-    converter.value = convValue;
-    convert();
+  if(converter.value !== '') {
+    if(one.textContent == '°C') {
+      one.textContent = '°F';
+      two.textContent = '°C';
+      converter.value = convValue.toFixed(1);
+      convert();
+    } else {
+      one.textContent = '°C';
+      two.textContent = '°F';
+      converter.value = convValue.toFixed(1);
+      convert();
+    }
   } else {
-    one.textContent = '°C';
-    two.textContent = '°F';
-    converter.value = convValue;
-    convert();
+    if(one.textContent == '°C') {
+      one.textContent = '°F';
+      two.textContent = '°C';
+    } else {
+      one.textContent = '°C';
+      two.textContent = '°F';
+    }
   }
 }
 
